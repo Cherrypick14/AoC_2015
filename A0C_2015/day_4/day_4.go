a9427faf602097bcb652f4850c9814075bbe4dfe
Cherrypick14 <cherrypicklet@gmail.com>
2024-09-04 13:29:53 +0300

    Implement brute-force solution for Adventcoin

---
+++ A0C_2015/day_4/day_4.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+)
+
+// This problem is a bruteforce search probem
+
+func FindAdventCoin(secretKey string) int {
+	count := 1 // we'll start with one as the lowest number.
+
+	for {
+		// concatenate the input string and the count.
+		data := fmt.Sprintf("%s%d", secretKey, count)
+
+		// generate the hash for it.
+
+		hash := md5.Sum([]byte(data))
+
+		// encode it to a string.
+
+		hashStr := hex.EncodeToString(hash[:])
+
+		// if the condition is met then return the count otherwise keep increasing it.
+
+		if hashStr[:5] == "00000" && hashStr[:6] == "000000" {
+			return count
+		}
+
+		count++
+	}
+}
+
+func main() {
+
+	word := "bgvyzdsv"
+
+	fmt.Println(FindAdventCoin(word))
+}
